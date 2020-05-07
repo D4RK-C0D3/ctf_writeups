6e15d2f209408bfa4ec06fdbe2d117987b72826d
dipannama <dipannama468@gmail.com>
2020-05-07 10:14:01 +0530

    adding new level solution & also modifing lev_gen file

---
+++ over_the_wire/bandit/level_8_COMPLETE/get_flag.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd inc/ && ./find_flag.sh 2> /dev/null && cat flag
+
---
+++ over_the_wire/bandit/level_8_COMPLETE/inc/find_flag.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sshpass -p cvX2JJa4CFALtqS87jk27qwqGhBM9plV ssh bandit8@bandit.labs.overthewire.org -p 2220  "bash -s" < script.sh > flag
+
